7b7d6155e46fe2e859c3508c89c29eaac3250e40
qobiljon1 <jumaboyevqobiljon17@gmail.com>
2022-10-05 20:27:07 +0500

    pravki

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React, {Component} from 'react';
 import {createRoot} from 'react-dom/client';
 import {useTranslation, initReactI18next} from "react-i18next";
-import i18n from "i18next";
 import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import HttpApi from "i18next-http-backend";
 import './index.css';
 
 i18n
     .use(initReactI18next) // passes i18n down to react-i18next
-    // .use(LanguageDetector)
+    .use(LanguageDetector)
+    .use(HttpApi)
     .init({
-        resources: {
-            en: {
-                translation: {
-                    "Welcome to React": "Welcome to React and react-i18next"
-                }
-            },
-            fr: {
-                translation: {
-                    "Welcome to React": "bir nimalar demoqchiman",
-                }
-            }
-        },
-        // lng: "fr", // if you're using a language detector, do not define the lng option
         fallbackLng: "fr",
         interpolation: {
             escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
@@ -29,7 +18,11 @@ i18n
         detection: {
             order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
             caches: ["localStorage"]
-        }
+        },
+        backend: {
+            loadPath: '/assets/locales/{{lng}}/translations.json'
+        },
+        react: {useSuspense: false}
     });
 
 function App() {
